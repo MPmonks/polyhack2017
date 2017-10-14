b634c34badeaa2463c68a4fcadabc736db6e7ccb
sdirie <32782238+sdirie@users.noreply.github.com>
2017-10-14 05:12:02 -0400

    flask is set up

--- flask_setup/app/run.sh
+++ flask_setup/app/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export FLASK_APP=app/__init__.py
+export FLASK_APP=__init__.py
 export FLASK_DEBUG=1
 flask run --port=3333 
--- flask_setup/app/views.py
+++ flask_setup/app/views.py
@@ -1,6 +1,28 @@
+from flask import render_template
 from app import app
+from flask import request
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return app.send_static_file('index.html')
+
+@app.route('/app.js')
+def appjs():
+	return app.send_static_file('app.js')
+
+"""
+@app.route('/name')
+def name():
+    user = {'nickname': 'Samsam'}  # fake user
+    return render_template('index.html',
+                           title='Home',
+                           user=user)
+"""
+
+@app.route('/give', methods=['POST'])
+def give():
+	f = open('test', 'w')
+	f.write(request.get_data())
+	f.close()
+	return ""
