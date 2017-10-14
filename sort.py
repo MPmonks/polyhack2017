16d176369dd861f6472ed9326f2d01adf4dfc498
jingqi <jingqi.yang@tufts.edu>
2017-10-13 23:27:54 -0400

    fixed tabbing

--- sort.py
+++ sort.py
@@ -10,29 +10,17 @@ def newEventInfo():
     estTimes = [] # list of estimated times needed for event completion
     deadlines = [] # list of event deadlines
 
-for i in range(0, num_events): # store event info from user input
-    names.append(raw_input("enter event name: "))
-    estTimes.append(input("enter estimated time to completion: "))
-    deadlines.append(input("enter deadline: "))
+    for i in range(0, num_events): # store event info from user input
+        names.append(raw_input("enter event name: "))
+        estTimes.append(input("enter estimated time to completion: "))
+        deadlines.append(input("enter deadline: "))
     
     userEvents = [] # store new events data in list
     for i in range(0, num_events):
         userEvents.append({"name": names[i], "estTime": estTimes[i], "deadline": deadlines[i]})
 
 def main():
-
-	raw_input(num_events)
-
-	num_events = 0
-	names[num_events]
-	estTimes[num_events]
-	deadlines[num_events]
-
-	for i in num_events:
-		names
-
-	#userEvents[
-
+    return
 
 if __name__ == '__main__':
 	main()
